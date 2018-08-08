fcebc5e65ac0888f2a69258779a2b7a44aaab4ea
swey01 <464748644@qq.com>
2018-08-08 16:50:34 +0800

    同步

---
+++ src/page/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../components/homeHeader.vue'
+export default {
+  name: 'home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Home from '@/page/home'
 
 Vue.use(Router)
 
@@ -8,8 +8,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      // 按需加载
+      component: () => import('@/page/home')
     }
   ]
 })
